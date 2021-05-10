4e7c11a6ad87ef3b8145f778ce3c4c8840d41d11
Ricardo Whaibe <whaibericardo@gmail.com>
2021-05-10 00:32:02 -0500

    Fixed payment routes

---
+++ routes/paymentService.js
@@ -0,0 +1,44 @@
+const { response } = require("express");
+const express = require("express");
+const router = express.Router();
+
+const loginController = require("../controllers/loginController");
+const registerController = require("../controllers/registerController");
+const groupController = require("../controllers/groupController");
+const studentController = require("../controllers/studentController");
+
+app.get("/", (req, res) => {
+  response.render("../views/checkout.html");
+});
+
+app.get("/success", (req, res) => {
+  response.render("../views/success.html");
+});
+app.get("/cancel", (req, res) => {
+  response.render("../views/cancel.html");
+});
+
+app.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: "Stubborn Attachments",
+            images: ["https://i.imgur.com/EHyR2nP.png"],
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `http://localhost:4242https://primaryschoolsystem.herokuapp.com/api/payment/success`,
+    cancel_url: `http://localhost:4242https://primaryschoolsystem.herokuapp.com/api/payment/cancel`,
+  });
+  res.json({ id: session.id });
+});
+
+module.exports = router;
