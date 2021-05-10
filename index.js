3ffe22fb182933bfdb32404df5682c76e158a42c
Ricardo Whaibe <whaibericardo@gmail.com>
2021-05-10 00:25:26 -0500

    Added payment setvices

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const loginRouter = require("./routes/login");
 const registerRouter = require("./routes/register");
 const adminRouter = require("./routes/admin");
 const studentRouter = require("./routes/student");
+const paymentRouter = require("./routes/paymentService");
+const stripe = require("stripe")(
+  "sk_test_51IpRGeErZiQYNN7iqOTsDAO15INLxcl8HnfhbuvoRhuU2nIKIBKVAmOInq6GQUiFnvt9r6O3ixvozEoch5TnM38o00av78NToI"
+);
+
 const mongoose = require("mongoose");
 const Joi = require("joi"); //Imports Joi package
 
@@ -32,6 +37,7 @@ app.use("/login", loginRouter);
 app.use("/api/register", registerRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/student", studentRouter);
+app.use("/api/payment", paymentRouter);
 
 //Open db connection
 const db = mongoose.connection;
@@ -40,6 +46,9 @@ db.on("error", console.error.bind(console, "Connection error"));
 db.once("open", () => {
   console.log("Connection established");
 });
+
+const YOUR_DOMAIN =
+  "http://localhost:4242https://primaryschoolsystem.herokuapp.com/";
 
 const port = process.env.PORT || 3000;
 //Restful api
